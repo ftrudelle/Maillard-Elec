f68c17efb75b662542aa6ce830f822cd63a75c39
Florian <trudelle.florian@gmail.com>
2024-09-10 21:16:53 +0200

    Modifying services list

--- views/services.php
+++ views/services.php
@@ -10,11 +10,22 @@
     <div class="row gy-4">
       <!-- Service Item -->
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+        <div class="service-item item-indigo position-relative">
+          <div class="icon">
+            <i class="fa-solid fa-wrench"></i>
+          </div>
+          <h3>Dépannage</h3>
+          <!-- <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p> -->
+        </div>
+      </div>
+      <!-- End Service Item -->
+      <!-- Service Item -->
+      <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
         <div class="service-item item-cyan position-relative">
           <div class="icon">
             <i class="fa-solid fa-house-chimney-crack"></i>
           </div>
-          <h3>Installation électrique, alimentation en courant electrique</h3>
+          <h3>Electricité</h3>
           <!-- <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p> -->
         </div>
       </div>
@@ -23,9 +34,9 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
         <div class="service-item item-orange position-relative">
           <div class="icon">
-            <i class="fa-solid fa-elevator"></i>
+            <i class="fa-solid fa-cogs"></i>
           </div>
-          <h3>Ascenseurs, monte-charge</h3>
+          <h3>Automatisme</h3>
           <!-- <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p> -->
         </div>
       </div>
@@ -45,9 +56,9 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
         <div class="service-item item-yellow position-relative">
           <div class="icon">
-            <i class="fa-solid fa-lightbulb"></i>
+            <i class="fa-solid fa-eye"></i>
           </div>
-          <h3>Eclairage, éclairage de sécurité, équipement d'éclairage exterieur aux batiments</h3>
+          <h3>Vidéo surveillance</h3>
           <!-- <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p> -->
         </div>
       </div>
@@ -56,20 +67,9 @@
       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
         <div class="service-item item-pink position-relative">
           <div class="icon">
-            <i class="fa-solid fa-tower-cell"></i>
+            <i class="fa-solid fa-house-signal"></i>
           </div>
-          <h3>Equipements de télécommunication</h3>
-          <!-- <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p> -->
-        </div>
-      </div>
-      <!-- End Service Item -->
-      <!-- Service Item -->
-      <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="700">
-        <div class="service-item item-red position-relative">
-          <div class="icon">
-            <i class="fa-solid fa-land-mine-on"></i>
-          </div>
-          <h3>Equipements électriques de securité</h3>
+          <h3>Domotique</h3>
           <!-- <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p> -->
         </div>
       </div>
