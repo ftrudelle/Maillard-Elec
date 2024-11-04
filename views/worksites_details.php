bb49405c9629f496ad017ac1288a30a008dae0b9
Florian <trudelle.florian@gmail.com>
2024-11-04 12:03:15 +0100

    Modifying worksite images to respect size of their container but not scale down in dimension

--- views/worksites_details.php
+++ views/worksites_details.php
@@ -4,24 +4,24 @@
 }
 
 // Récupération du contenu du fichier JSON
-$json_content = file_get_contents("../" . $description);
+$jsonContent = file_get_contents("../" . $description);
 // Décodage du fichier JSON
-$worksite_description = json_decode($json_content);
+$worksiteDescription = json_decode($jsonContent);
 
 //Récupération des images du chantier
-$worksite_images = glob("../" . $images . '/*');
+$worksiteImages = glob("../" . $images . '/*');
 
 // Création des images dans l'article
 $image_string = "";
-foreach ($worksite_images as $worksite_image) {
-  $image_string .= '<div class="swiper-slide"><img src="' . substr($worksite_image, 3) . '" alt=""></div>';
+foreach ($worksiteImages as $worksiteImage) {
+  $image_string .= '<div class="swiper-slide"><img src="'.substr($worksiteImage, 3).'" alt="" style="width:715px;height:535px;object-fit: contain;"></div>';
 }
 // Envoi des articles de chantier dans la page
 echo '<!-- worksite Details Section -->
     <section id="worksite-details" class="worksite-details section">
       <!-- Section Title -->
       <!--<div class="container section-title" data-aos="fade-up">
-        <h2>' . $worksite_description->titre . '</h2>
+        <h2>' . $worksiteDescription->titre . '</h2>
         <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
       </div>-->
       <!-- End Section Title -->
@@ -36,7 +36,7 @@ echo '<!-- worksite Details Section -->
           <div class="col-lg-4">
             <div class="worksite-description" data-aos="fade-up" data-aos-delay="300">
               <h2>Description</h2>
-              <p>' . $worksite_description->description . '</p>
+              <p>' . $worksiteDescription->description . '</p>
             </div>
           </div>
         </div>
