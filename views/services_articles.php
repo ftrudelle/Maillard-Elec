0a41902dccdfe8637f05d315c583b62f3693a11e
Florian <trudelle.florian@gmail.com>
2024-11-04 11:28:12 +0100

    Adding order to services to be sorted

--- views/services_articles.php
+++ views/services_articles.php
@@ -2,28 +2,37 @@
 // Récupération des dossiers de chantiers
 $services = glob("services/*");
 
+$serviceArticles = array();
+
+// Création des services
 foreach ($services as $service) {
   //Ignore description_form.json
   if($service == "services/description_form.json"){
     continue;
   }
-  // Récupération du contenu du fichier JSON
-  $json_content = file_get_contents($service);
-  // Décodage du fichier JSON
-  $service_infos = json_decode($json_content);
+  
+  // Récupération du contenu du fichier JSON et décodage du fichier JSON
+  $serviceInfos = json_decode(file_get_contents($service));
 
-  // Envoi des articles de chantier dans la page
-  echo '<!-- Service Item -->
-      <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-        <div class="service-item position-relative">
-          <div class="icon">
-            <i class="fa-solid '.$service_infos->icon.'" style="color:'.$service_infos->icon_color.';"></i>
-          </div>
-          <h3>'.$service_infos->titre.'</h3>
-          <div class="service-description">
-            <p>'.$service_infos->description.'</p>
-          </div>
-        </div>
+  $serviceItem = '<!-- Service Item -->
+  <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+    <div class="service-item position-relative">
+      <div class="icon">
+        <i class="fa-solid '.$serviceInfos->icon.'" style="color:'.$serviceInfos->icon_color.';"></i>
       </div>
-      <!-- End Service Item -->';
+      <h3>'.$serviceInfos->titre.'</h3>
+      <div class="service-description">
+        <p>'.$serviceInfos->description.'</p>
+      </div>
+    </div>
+  </div>
+  <!-- End Service Item -->';
+
+  $serviceArticles[$serviceInfos->order] = $serviceItem;
+}
+
+ksort($serviceArticles);
+// Envoi des articles de chantier dans la page
+foreach($serviceArticles as $service){
+  echo $service;
 }
