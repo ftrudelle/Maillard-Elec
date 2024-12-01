1c784cd84201327bf6e92c32ce9513f259d91039
Florian <trudelle.florian@gmail.com>
2024-12-01 11:18:37 +0100

    Testing indexes and increment it when one is the same

--- views/services_articles.php
+++ views/services_articles.php
@@ -1,19 +1,19 @@
 <?php
 // Récupération des dossiers de chantiers
 $services = glob("services/*");
-
 $serviceArticles = array();
 
 // Création des services
 foreach ($services as $service) {
   //Ignore description_form.json
-  if($service == "services/description_form.json"){
+  if($service == "services/service_exemple.json"){
     continue;
   }
   
   // Récupération du contenu du fichier JSON et décodage du fichier JSON
   $serviceInfos = json_decode(file_get_contents($service));
 
+  $index = getFreeIndexInArrayByIndex($serviceInfos->order, $serviceArticles);
   $serviceItem = '<!-- Service Item -->
   <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
     <div class="service-item position-relative">
@@ -28,11 +28,21 @@ foreach ($services as $service) {
   </div>
   <!-- End Service Item -->';
 
-  $serviceArticles[$serviceInfos->order] = $serviceItem;
+  $serviceArticles[$index] = $serviceItem;
 }
 
 ksort($serviceArticles);
 // Envoi des articles de chantier dans la page
 foreach($serviceArticles as $service){
   echo $service;
+}
+
+//Function that increment index if it's alreay taken in an array and return it if it's not
+function getFreeIndexInArrayByIndex($index, $array){
+  if(!array_key_exists($index, $array)){
+    return $index;
+  }
+  else{
+    return getFreeIndexInArrayByIndex($index + 1, $array);
+  }
 }
